2ee3afb6fe0385ab00e1132dab2f5d4eb2a1a2e7
Prerana <vprerana23@gmail.com>
2026-01-25 12:19:59 +0530

    Linkde list with OOP

---
+++ LinkedList/linklistOOP.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void insert(int value) {
+        Node* newNode = new Node(value);
+
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << "-> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    int n, value;
+
+    cout << "Enter number of elements in linked list: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i << ": ";
+        cin >> value;
+        list.insert(value);
+    }
+
+    cout << "The list is: ";
+    list.display();
+
+    return 0;
+}
